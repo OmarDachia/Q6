30d05414209a35a56337cebf6486187a4c1464bc
Omar Dachia <omardachia@gmail.com>
2024-04-02 11:01:08 +0100

    #Happy Happy

---
+++ Q.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  cout << "#5:-> Write a program to find average of 7 numbers.";
+  cout << "\n=========================================================================================";
+  cout << "\nProgram Start here:->" << endl;
+  cout << "\n++++++++++++++++++++\n";
+
+  double sub1 = 0, sub2 = 0, sub3 = 0, sub4 = 0, sub5 = 0,sub6= 0, sub7 = 0, Average = 0;
+  cout << "Type Number 1: ";
+  cin >> sub1;
+
+  cout << "Type the Number 2: "<<endl;
+  cin >> sub2;
+
+  cout << "Type the Number 3: ";
+  cin >> sub3;
+
+  cout << "Type Number 4: ";
+  cin >> sub4;
+
+  cout << "Type the Number 5: "<<endl;
+  cin >> sub5;
+
+  cout << "Type Number 6: ";
+  cin >> sub6;
+
+  cout << "Type the Number 7: "<<endl;
+  cin >> sub7;
+
+
+  Average = ((sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7)/7);
+
+
+  cout << "Average "<< Average <<endl;
+
+  return 0;
+}
+
+
+
